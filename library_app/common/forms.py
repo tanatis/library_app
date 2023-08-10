9da40f394431271662f3c6ec56369c2808b84df1
Ivan Pekov <64958860+tanatis@users.noreply.github.com>
2023-08-10 16:49:47 +0300

    fixed last viewed book
    users can see only their last viewed books

--- library_app/common/forms.py
+++ library_app/common/forms.py
@@ -2,16 +2,6 @@ from django import forms
 
 
 class SearchForm(forms.Form):
-    # book = forms.CharField(
-    #     required=False,
-    #     label='',
-    #     widget=forms.TextInput(
-    #         attrs={
-    #             'placeholder': 'Search book ...',
-    #             'class': 'search-input',
-    #         }
-    #     )
-    # )
     SEARCH_CHOICES = (
         ('title', 'Search by book title'),
         ('author', 'Search by author'),
@@ -21,7 +11,6 @@ class SearchForm(forms.Form):
         choices=SEARCH_CHOICES,
         widget=forms.RadioSelect,
         label='',
-        #initial='title',  # Set the default search criteria to 'title'
     )
 
     query = forms.CharField(
